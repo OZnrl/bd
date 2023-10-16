b14f4bc45fc972239680f062a7116f0f1cdb841b
OZnrl <108094114+OZnrl@users.noreply.github.com>
2023-10-17 00:09:55 +0200

    Update listVideos.php

--- BrainDir/listVideos.php
+++ BrainDir/listVideos.php
@@ -1,40 +1,24 @@
 <?php
-// Connect to the database
 $db = new mysqli('localhost', 'root', '', 'braindb');
-
 if ($db->connect_error) {
     die("Connection failed: " . $db->connect_error);
 }
-
-// Fetch all filenames from the database
 $sql = "SELECT titles FROM files";
 $result = mysqli_query($db, $sql);
-
-// Check if there are rows in the result
 if (mysqli_num_rows($result) > 0) {
-   
     if ($result->num_rows > 0) {
-        // Output data of each row
         while ($row = $result->fetch_assoc()) {
-            // Print each column in the row
             foreach ($row as $column => $value) {
                 if (preg_match('/\.(mp3|mp4)$/', $value)) {
-                    /* echo '<ul style="list-style-type: none;display: flex;flex-direction: column;align-items: baseline; overflow: hidden;">'; */
                     echo '<li>' . htmlspecialchars($row['titles']) . '</li>';
-                    echo '<hr>';
-                  
+                    echo '<hr>';  
                 } else {
-                
                 }
             }
-            /* echo "<hr>"; // Add a separator between rows */
         }
     }    
-    } else {
-        echo "No data found.";
-    }
-
-
-// Close the database connection
+} else {
+    echo "No data found.";
+}
 $db->close();
 ?>
